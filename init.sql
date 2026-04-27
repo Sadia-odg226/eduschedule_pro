fe16d6d90d898dc4cc23bc9b5e709005ee585795
OUEDRAOGO Sadia <sadiaodg4@gmail.com>
2026-04-27 21:08:54 +0000

    ajouter des test de données

--- init.sql
+++ init.sql
@@ -154,7 +154,7 @@ CREATE TABLE logs_activite (
     date_heure TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );*/
 
--- INTSER: ajout des données
+-- INTSER: ajout des tests données
 
 
 INSERT INTO classes (code, libelle, niveau) VALUES
