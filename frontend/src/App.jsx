1afc16ec6fc242d4772ab38d41b2e8fb26384ed3
alexianeKMG <alexianekalmogho@gmail.com>
2026-04-28 23:24:06 +0000

    création de layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ export default function App() {
   return (
     <Layout>
       <h1 className="text-primary">🎓 EduSchedule Pro</h1>
-      <p>Application en cours de construction...</p>
     </Layout>
   )
 }
