454cefa5149ccbc9cb12fe5fb08b7210271b8570
alexianeKMG <alexianekalmogho@gmail.com>
2026-04-28 19:40:22 +0000

    projet react installé

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-primary"> EduSchedule Pro</h1>
+      <p>Application en cours de construction...</p>
+    </div>
+  )
+}
